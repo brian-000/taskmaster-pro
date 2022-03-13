f1606172dcbbe818e7fb01f80ee203ad77690248
Brain <ortiz_brian323@yahoo.com>
2022-03-12 21:55:38 -0800

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,7 +160,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
 	// get form values
 	var taskText = $("#modalTaskDescription").val();
 	var taskDate = $("#modalDueDate").val();
@@ -196,16 +196,20 @@ $(".card .list-group").sortable({
 	tolerance: "pointer",
 	helper: "clone",
 	activate: function (event) {
-		console.log("activate", this);
+		$(this).addClass("dropover");
+		$(".bottom-trash").addClass("bottom-trash-drag");
 	},
 	deactivate: function (event) {
-		console.log("deactivate", this);
+		$(this).removeClass("dropover");
+		$(".bottom-trash").removeClass("bottom-trash-drag");
 	},
 	over: function (event) {
-		console.log("over", event.target);
+		$(this).addClass("dropover-active");
+		$(".bottom-trash").addClass("bottom-trash-active");
 	},
 	out: function (event) {
-		console.log("out", event.target);
+		$(this).removeClass("dropover-active");
+		$(".bottom-trash").removeClass("bottom-trash-active");
 	},
 	update: function (event) {
 		// array to store the task data in
@@ -283,4 +287,9 @@ var auditTask = function (taskEl) {
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function () {
+	$(".card .list-group-item").each(function(index, el) {
+	  auditTask(el);
+	});
+	//console.log(taskEl)
+     }, (1000 * 60) * 30);
